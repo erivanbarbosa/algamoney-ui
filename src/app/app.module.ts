99f2a4f5769361737f8927e7a98669320094fdeb
erivan <erivan.nas@gmail.com>
2018-05-18 09:46:16 -0300

    Primeiro Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {InputTextModule} from 'primeng/inputtext';
+import {ButtonModule} from 'primeng/button';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,8 +9,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
-  ],
+    BrowserModule,
+    InputTextModule,
+    ButtonModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
