ce33c14c7f679a74ea1b9f949ceec57f75eea073
Erivan <erivan.nas@gmail.com>
2018-05-21 08:35:32 -0300

    Mais mudanças

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,22 @@ import {InputTextModule} from 'primeng/inputtext';
 import {ButtonModule} from 'primeng/button';
 import { AppComponent } from './app.component';
 import {DataTableModule, DataTable} from 'primeng/datatable';
+import {TooltipModule} from 'primeng/tooltip';
+import { LancamentosPesquisaComponent } from './lancamentos-pesquisa/lancamentos-pesquisa.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LancamentosPesquisaComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     InputTextModule,
     ButtonModule,
-    DataTableModule
+    DataTableModule,
+    TooltipModule
     ],
   providers: [],
   bootstrap: [AppComponent]
