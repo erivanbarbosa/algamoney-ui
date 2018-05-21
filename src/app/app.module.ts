f25a272ed118cf9ed8278ede135ef296dde4f994
Erivan <erivan.nas@gmail.com>
2018-05-20 21:19:42 -0300

    Tabela

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import {InputTextModule} from 'primeng/inputtext';
 import {ButtonModule} from 'primeng/button';
 import { AppComponent } from './app.component';
+import {DataTableModule, DataTable} from 'primeng/datatable';
 
 @NgModule({
   declarations: [
@@ -11,7 +12,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     InputTextModule,
-    ButtonModule
+    ButtonModule,
+    DataTableModule
     ],
   providers: [],
   bootstrap: [AppComponent]
